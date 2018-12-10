40b6050cecc6809e33591caa4740e1182b3bcf30
Deaol <45747910+Deaol@users.noreply.github.com>
2018-12-10 07:02:34 +0300

    firstcom

---
+++ hl.py
@@ -0,0 +1,22 @@
+from tkinter import *
+import turtle
+
+
+root = Tk()
+
+def bu():
+    x = int(x_entry.get())
+    y = int(y_entry.get())
+    turtle.setpos(x, y)
+    turtle.exitonclick()
+
+
+button = Button(text="", command=bu)
+button.grid(row=2, column=1, padx=5, pady=5, sticky="e")
+x_entry = Entry()
+y_entry = Entry()
+x_entry.grid(row=0, column=1, padx=5, pady=5)
+y_entry.grid(row=1, column=1, padx=5, pady=5)
+
+root.mainloop()
+
