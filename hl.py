82aa2b9db4fa665542b895fc4c4acf1bda0fe7a4
Deaol <45747910+Deaol@users.noreply.github.com>
2018-12-10 07:31:02 +0300

    Update hl.py

--- hl.py
+++ hl.py
@@ -12,7 +12,7 @@ def bu():
 
 
 button = Button(text="", command=bu)
-button.grid(row=2, column=1, padx=5, pady=5, sticky="e")
+button.grid(row=2, column=1, padx=5, pady=5, sticky="тык")
 x_entry = Entry()
 y_entry = Entry()
 x_entry.grid(row=0, column=1, padx=5, pady=5)
